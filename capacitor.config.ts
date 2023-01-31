5f346e21618617e31b203271db437104c7dffe7a
AlejandroBenSan <alexbbssforma@gmail.com>
2023-01-31 20:15:15 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'SeriesAppMulti',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
